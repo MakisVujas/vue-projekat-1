59d5511770fd786a0b1fe66b332bd59cad1d3d2c
marko <vujas.m@gmail.com>
2024-01-06 12:21:48 +0100

    sminka

--- vue-projekat-1/src/App.vue
+++ vue-projekat-1/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 
 import Counter from './components/Counter.vue'
+import PlusAndMinus from './components/PlusAndMinus.vue'
 
 </script>
 
---
+++ vue-projekat-1/src/components/PlusAndMinus.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <button>+</button>
+        <button>-</button>
+    </div>
+</template>
